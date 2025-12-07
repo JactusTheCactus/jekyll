e5f060aa70f41a20011d4f9102e40fb252210fcb
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-07 04:54:53 -0800

    Update pre.sh

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
 alias yq="yq --yaml-fix-merge-anchor-to-spec=true"
-exec > logs/pre.log 2>& 1
-data="$(yq -p yaml -o json data.yml | jq "del(.[0])")"
-echo "$data" | jq -c '.[]' | while read -r i; do
-	echo "$i" | jq -c '.' | while read -r m; do
-		m="$(echo "$m" | jq --tab .)"
+exec > "logs/pre.log" 2>& 1
+data="$(yq -p yaml -o json "data.yml" | jq "del(.[0])")"
+echo "$data" | jq -c ".[]" | while read -r i; do
+	echo "$i" | jq -c "." | while read -r m; do
+		m="$(echo "$m" | jq --tab ".")"
 		re=(
 			's/"\]/",1]/g'
 			's/,(\d)/, $1/g'
@@ -47,16 +47,8 @@ echo "$data" | jq -c '.[]' | while read -r i; do
 		g_="$(echo "$m" | jq -r ".name")"
 		g+="custom_model_data={strings:[${g_,,}]},"
 		g+="consumable={consume_seconds:0}"
-		t="dist/"
-		t+="datapacks/"
-		t+="Project: Jekyll/"
-		t+="data/"
-		t+="jekyll/"
-		t+="function/"
-		t+="mob/"
 		t_="$(echo "$m" | jq -r ".name")"
-		t+="${t_,,}/"
-		t+="give.mcfunction"
+		t="dist/datapacks/Project: Jekyll/data/jekyll/function/mob/${t_,,}/give.mcfunction"
 		echo "give @p minecraft:dragon_breath[$g]" > "$t"
 	done
 done
