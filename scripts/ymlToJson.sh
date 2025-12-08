64f7fdff9f54fd5a30ef1d2b499dfc165c8d0cd6
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-12-07 22:04:12 -0800

    -

--- scripts/ymlToJson.sh
+++ scripts/ymlToJson.sh
@@ -2,15 +2,11 @@
 shopt -s expand_aliases
 alias yq="yq --yaml-fix-merge-anchor-to-spec=true"
 exec >> logs/yml.log 2>& 1
-i="$1"
+i="${1#./}"
 i="${i%.yml}"
-i="${i#./}"
 if ! grep -qw "$i" .ymlignore; then
-	o="$(echo "${1/src/dist}" \
-		| perl -pe '
-			s/yml/json/g;
-			s/(\.\w+)\.json/$1/g;
-		'
-	)"
+	o="${1/src/dist}"
+	o="${o/yml/json}"
+	o="$(echo "$o" | perl -pe 's/(\.\w+)\.json/$1/g')"
 	yq -p yaml -o json "$1" > "$o"
 fi
